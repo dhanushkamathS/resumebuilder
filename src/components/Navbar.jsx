a339b0091a3f7da3387b23eb5d75306d63d4c863
dhanush <dhanushkamath@gmail.com>
2023-07-11 19:32:48 +0530

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,10 +24,10 @@ const Navbar = () =>{
     <div className={`${toggle?"":"hidden"} w-full md:block md:w-auto`} id="navbar-default">
       <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border space-y-5 md:space-y-0 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-          <Link to={'/'}  className="block py-2 pl-3 pr-4 text-sm md:text-lg text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
+          <Link to={'/'}  className="block py-2 pl-3 pr-4 text-sm md:text-lg text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 " aria-current="page">Home</Link>
         </li>
         <li>
-          <Link to={'/'} onClick={setDefault} className="block text-sm md:text-lg py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Reset</Link>
+          <Link to={'/'} onClick={setDefault} className="block text-sm md:text-lg py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0" aria-current="page">Reset</Link>
         </li>
       </ul>
     </div>
--- src/pages/personalDetail/index.jsx
+++ src/pages/personalDetail/index.jsx
@@ -52,7 +52,7 @@ const PersonalDetailInput = ({formData,setFormData}) =>{
                     <input type="text" id="first_name" value={formData.firstName} onChange={(e)=>{handleFormChange(e)}} name='firstName' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required/>
                 </div>
                 <div>
-                    <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last name</label>
+                    <label  className="block mb-2 text-sm font-medium text-white">Last name</label>
                     <input type="text" id="last_name" value={formData.lastName} onChange={(e)=>{handleFormChange(e)}} name='lastName' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required/>
                 </div>
                 <div>
