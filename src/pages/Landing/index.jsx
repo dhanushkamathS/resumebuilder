e8f88e99bb8c7d56beb12240e1cd223c47fa8d9c
dhanush <dhanushkamath@gmail.com>
2023-07-23 07:56:37 +0530

    added vercel config

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Langing() {
   return (
-    <div className='bg-pink-300 h-screen overflow-hidden'>
+    <div className='bg-black h-screen overflow-hidden'>
         <iframe className=' overflow-auto' src="https://breathtaking-status-108040.framer.app/"   frameBorder="0" width={"101%"} height={"100%"}></iframe>
     </div>
   )
